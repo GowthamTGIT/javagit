49d7c4a262d6a3e23625a7550d13bc68c53d309c
GowthamTGIT <gowthamabimanu@gmail.com>
2023-01-31 22:45:36 +0530

    mytask

---
+++ src/org/client/ClientDetails.java
@@ -0,0 +1,21 @@
+package org.client;
+
+public class ClientDetails 
+{
+public void ClientId()
+{
+System.out.println("ClientId : 5223");
+}
+public void ClientName()
+{
+System.out.println("ClientName : krish");
+}
+public static void main(String[] args)
+{
+	ClientDetails c = new ClientDetails();
+	c.ClientId();
+	c.ClientName();
+
+	
+}
+}
